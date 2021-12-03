c43e8844c75b4e3167571e32423e39594789a022
Jakobe16 <jakostom20@gmail.com>
2021-12-03 12:02:59 -0600

    Initial Commit

---
+++ Learning App 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning App 2
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 12/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning App 2/Learning_App_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_App_2App.swift
+//  Learning App 2
+//
+//  Created by PM JavaScript - Ja'kobe Stumon on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_App_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
